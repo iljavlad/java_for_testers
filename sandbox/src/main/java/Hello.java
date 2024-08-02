ca2ade769a0b2f9532ada34b57a8124607963030
iljavlad <iljavlad@gmail.com>
2024-08-02 20:20:26 +0500

    Замена на классы Record

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,8 +1,14 @@
+import java.io.File;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
 
         System.out.println(2+2*2);
         System.out.println("Hello," + "world!");
+
+        var configFile = new File("sandbox/build.gradle");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.exists());
     }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -7,12 +7,13 @@ public class SquareTests {
 
     @Test
     void canCalculadeArea() {
-        double result = Square.Area(5.0);
+        var s = new Square(5.0);
+        double result = s.area();
         Assertions.assertEquals(25.0, result);
     }
 
     @Test
     void canCalculatePerimeter() {
-        Assertions.assertEquals(20.0, Square.perimetr(5.0));
+        Assertions.assertEquals(20.0, new Square(5.0).perimetr());
     }
 }
