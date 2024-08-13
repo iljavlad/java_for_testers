4370cec6a9d6d033fef74e5d2668272519211c8c
iljavlad <iljavlad@gmail.com>
2024-08-13 18:29:59 +0500

    Функция equals
    Сравнение треугольников

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
@@ -38,4 +38,11 @@ public class TriangleTest {
             //OK
         }
     }
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
 }
